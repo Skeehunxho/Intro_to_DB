ed2406ca715daea613353a9d66e134cf0ed7b8e4
Kent Muia <Kentmmmuia18@gmail.com>
2025-08-05 14:51:34 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -2,6 +2,4 @@
 USE alx_book_store;
 
 -- List all tables in the alx_book_store database
-SELECT TABLE_NAME 
-FROM INFORMATION_SCHEMA.TABLES 
-WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG = 'alx_book_store';
+SHOW TABLES;
